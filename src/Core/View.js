b2c2b506825b80855a9996eea22f64ea095340fc
Éric Lemoine <eric.lemoine@oslandia.com>
2018-04-09 16:26:24 +0200

    Move the frozen layer property to the base class

--- src/Core/View.js
+++ src/Core/View.js
@@ -139,7 +139,6 @@ function _preprocessLayer(view, layer, provider, parentLayer) {
     if (layer.type == 'geometry' || layer.type == 'debug') {
         defineLayerProperty(layer, 'visible', true,
                             () => _syncGeometryLayerVisibility(layer, view));
-        defineLayerProperty(layer, 'frozen', false);
         _syncGeometryLayerVisibility(layer, view);
     }
 
